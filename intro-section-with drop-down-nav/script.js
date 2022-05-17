85e5ad6be268b0268392916eb996c8644682c32b
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-05-17 02:00:54 +0100

    added mobile ui

--- intro-section-with drop-down-nav/script.js
+++ intro-section-with drop-down-nav/script.js
@@ -1,1 +1,30 @@
+const menuMobile = document.getElementById('menu-mobile')
+const dropdownMobile = document.getElementsByClassName('dropdown-m')
 
+window.matchMedia("(min-width: 769px)").addEventListener('change', function (event) {
+    if (event.target.matches) {
+        menuMobile.style.display = "none";
+    }
+});
+
+document.getElementById('menu').onclick = () => {
+    menuMobile.style.display = "block";
+}
+
+document.getElementById('menu-close').onclick = () => {
+    menuMobile.style.display = "none";
+    for (let i = 0; i < dropdownMobile.length; i++) {
+        dropdownMobile[i].nextElementSibling.style.display = "none"
+    }
+}
+
+for (let i = 0; i < dropdownMobile.length; i++) {
+    dropdownMobile[i].addEventListener("click", function () {
+        let dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none"
+        } else {
+            dropdownContent.style.display = "block"
+        }
+    })
+}
